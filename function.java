f75f3ac3a9985f2e22844e27defaaaa885765c3a
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2026-04-22 00:07:53 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,12 @@
+public class function{
+    public static void printHelloWorld(){
+        System.out.println("Hello shraddha");
+        System.out.println("Hello shraddha");
+        System.out.println("Hello shraddha");
+    }
+public static void main(String[] args){
+    printHelloWorld();
+    
+
+}
+}
