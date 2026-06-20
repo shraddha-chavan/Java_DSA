58c0d3a4fcc4c05098ad08bd1378d0d2d56b8ec8
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2026-06-20 12:18:43 +0530

    Update ContinueExample.java

--- ContinueExample.java
+++ ContinueExample.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ContinueExample {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         
         do {
             System.out.println("Enter no.");
